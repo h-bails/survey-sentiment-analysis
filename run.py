7ab8b4bbf3ecfc16533ebd00607ada5a42a0112f
h-bails <hollyannebailey@gmail.com>
2023-02-12 13:24:41 +0000

    Fix linter errors and warnings

--- run.py
+++ run.py
@@ -1,13 +1,13 @@
+from collections import Counter
+import pathlib
 import gspread
 from google.oauth2.service_account import Credentials
 from tabulate import tabulate
-from collections import Counter
 import spacy
 from spacy.matcher import Matcher
 from spacytextblob.spacytextblob import SpacyTextBlob
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
-import pathlib
 
 nlp = spacy.load('en_core_web_sm')
 nlp.add_pipe('spacytextblob')
@@ -28,7 +28,7 @@ SHEET = GSPREAD_CLIENT.open("sentiment-analysis-data-set")
 
 def fetch_headers():
     """
-    Displays the data available for analysis by arranging headers into a dict, 
+    Displays the data available for analysis by arranging headers into a dict,
     then tabulating the dict using the tabulate module.
     """
     data = SHEET.worksheet('data').get_all_values()
@@ -37,8 +37,9 @@ def fetch_headers():
     header_dict = {}
     for title, num in zip(header_title, header_num):
         header_dict[title] = num
-    print(tabulate(header_dict.items(), headers = ["Question", "Option"]))
-    print("\nTo begin analysing answers to a question,\nenter its allocated number followed by Enter.\n")
+    print(tabulate(header_dict.items(), headers=["Question", "Option"]))
+    print("\nTo begin analysing survey data,")
+    print("enter its allocated number followed by Enter.\n")
 
     while True:
         try:
@@ -46,45 +47,48 @@ def fetch_headers():
             if header_choice in ["1", "2", "3", "4", "5"]:
                 break
             else:
-                raise ValueError("Number not in list")
-        except ValueError as e:
-            print(f"{e}: Invalid selection. Please try again.")
+                raise ValueError("Invalid selection")
+        except ValueError as error:
+            print(f"{error}: Number not in list. Please try again.")
             continue
-            
-    print("Great! Let's analyse your data!/n")          
+
+    print("Great! Let's analyse your data!\n")
     return header_choice
 
 
 def get_selected_data(selection):
     """
-    Fetches the data from the specified column and concatenates it into a single string for analysis.
+    Fetches the data from the specified column and
+    concatenates it into a single string for analysis.
     Prepares the string for sentiment analysis.
     """
     worksheet = SHEET.worksheet('data')
     data_to_analyse = worksheet.col_values(selection)
     data_string = ' '.join(data_to_analyse)
-    
+
     return data_string
 
 
 def analyse_themes(string):
     """
-    Tokenises and lemmatizes the words in the string, assigning them lemma values.
+    Tokenises and lemmatizes the words in the string,
+    assigning them lemma values.
     Ignores stop words and punctuation.
     Analyses the string for frequency of words and phrases.
     Assigns a sentiment score to the answers to the question.
     """
-    doc = nlp(string, disable = ['ner'])
-    words = [token.lemma_ for token in doc if not token.is_stop and not token.is_punct]
+    doc = nlp(string, disable=['ner'])
+    words = [token.lemma_ for token in doc if not
+             token.is_stop and not token.is_punct]
     word_freq = Counter(words).most_common(10)
-    
+
     lemmatized_string = ' '.join(words)
-    phrase_doc = nlp(lemmatized_string, disable = ['ner'])
-    matcher = Matcher(nlp.vocab) 
-    pattern = [{'POS':'ADJ'}, {'POS':'NOUN'}] 
-    matcher.add('ADJ_PHRASE', [pattern]) 
-    phrase_matches = matcher(phrase_doc, as_spans=True) 
-    phrases = [] 
+    phrase_doc = nlp(lemmatized_string, disable=['ner'])
+    matcher = Matcher(nlp.vocab)
+    pattern = [{'POS': 'ADJ'}, {'POS': 'NOUN'}]
+    matcher.add('ADJ_PHRASE', [pattern])
+    phrase_matches = matcher(phrase_doc, as_spans=True)
+    phrases = []
     for span in phrase_matches:
         phrases.append(span.text.lower())
     phrase_freq = Counter(phrases).most_common(10)
@@ -105,28 +109,33 @@ def analyse_themes(string):
         print("\nSentiment of responses skewed towards *Positive*.\n")
     elif round(sentiment_score) == 0:
         print("\nSentiment of responses was largely *Neutral*.\n")
-    
-    print("What do you want to do next?/n")
-    
+
+    print("What do you want to do next?\n")
+
     while True:
         try:
-            word_cloud_choice = input("Enter 1 to build a word cloud of this data. Enter 2 to analyze more data.")
-            if word_cloud_choice == 1:
-                print("OK! Now building your Word Cloud.../n")
-                build_word_cloud(doc)
+            word_cloud_choice = input("Enter 1 to build a word cloud of this\
+                data, or 2 to analyze more data. ")
+            if int(word_cloud_choice) == 1:
+                print("OK! Building your Word Cloud...\n")
+                build_word_cloud(phrases)
                 break
-            elif word_cloud_choice == 2:
-                print("OK! Taking you back to the home screen.../n")
+            elif int(word_cloud_choice) == 2:
+                print("OK! Taking you back to the home screen...\n")
                 main()
                 break
             else:
                 raise ValueError("Invalid selection")
-        except ValueError as e:
-            print(f"{e}: Available options are 1 or 2. Please try again.")
+        except ValueError as error:
+            print(f"{error}: Available options are 1 or 2. Please try again.")
             continue
 
 
 def build_word_cloud(string):
+    """
+    Builds a word cloud out of most common phrases in
+    selected data category
+    """
     phrases_string = ' '.join(string)
     wordcloud = WordCloud().generate(phrases_string)
     plt.imshow(wordcloud)
@@ -141,15 +150,19 @@ def build_word_cloud(string):
     wordcloud_sheet = SHEET.worksheet('WordCloud')
     wordcloud_sheet.update_cell(1, 1, f'=IMAGE("{file_path}")')
 
- 
+
 def main():
+    """
+    Run all program functions
+    """
     header_choice = fetch_headers()
     data_string = get_selected_data(header_choice)
     analyse_themes(data_string)
 
 
 print("Welcome to the Survey Sentiment Analyser!\n")
-print("This program can help you perform sentiment analysis on\nopen-text responses on a Google Sheet.")
+print("This program can help you perform sentiment analysis on")
+print("open-text survey responses in a Google Sheet.\n")
 input("Press Enter to begin the data analysis.\n")
 
 print("Fetching available data categories...\n")
